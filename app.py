f3422ef9672f5b52b339aebe80891b6d536e9166
afcu0283 <msalmastour@alruwad.net>
2025-07-09 09:49:21 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import uuid
 import datetime
 
 app = Flask(__name__)
 complaints = {}
+admin_username = 'admin'
+admin_password = '123456'
 
 @app.route('/')
 def home():
@@ -20,7 +22,8 @@ def submit():
         'phone': phone,
         'message': message,
         'status': 'قيد المراجعة',
-        'time': str(datetime.datetime.now())
+        'time': str(datetime.datetime.now()),
+        'reply': ''
     }
     return render_template('submitted.html', complaint_id=complaint_id)
 
@@ -29,3 +32,22 @@ def track():
     track_id = request.form.get('track_id')
     complaint = complaints.get(track_id)
     return render_template('track.html', complaint=complaint, track_id=track_id)
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == admin_username and password == admin_password:
+            return render_template('dashboard.html', complaints=complaints)
+        else:
+            return "بيانات الدخول غير صحيحة"
+    return render_template('admin_login.html')
+
+@app.route('/reply/<complaint_id>', methods=['POST'])
+def reply(complaint_id):
+    reply_message = request.form.get('reply')
+    if complaint_id in complaints:
+        complaints[complaint_id]['reply'] = reply_message
+        complaints[complaint_id]['status'] = 'تم الرد'
+    return redirect(url_for('admin'))
